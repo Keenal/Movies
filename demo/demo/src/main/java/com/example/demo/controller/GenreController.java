e6e97ef97e06f58911158b7ecd93980a24b86616
Keenal <keenal.shah@ufl.edu>
2020-12-26 19:21:22 -0600

    add get methods
    Signed-off-by: Keenal <keenal.shah@ufl.edu>

--- demo/demo/src/main/java/com/example/demo/controller/GenreController.java
+++ demo/demo/src/main/java/com/example/demo/controller/GenreController.java
@@ -1,11 +1,22 @@
 package com.example.demo.controller;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Collection;
+import java.util.Optional;
 
 import com.example.demo.model.Genre;
 import com.example.demo.repository.GenreRepository;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +32,15 @@ public class GenreController {
 
     @GetMapping("/genres")
     Collection<Genre> genres() {
-        return genreRepository.findAll(); //select * from genre
+        return genreRepository.findAll(); // select * from genre
     }
+
+    @GetMapping("/genres/{id}")
+    ResponseEntity<?> getGenre(@PathVariable Long id) {
+        Optional<Genre> genre = genreRepository.findById(id);
+        return genre.map(response -> ResponseEntity.ok().body(response))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    
 }
