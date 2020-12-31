450203a9fe6f8041a2930bdabdd88c23a04e4ec6
Keenal <keenal.shah@ufl.edu>
2020-12-30 22:29:42 -0600

    add movie form
    Signed-off-by: Keenal <keenal.shah@ufl.edu>

--- app/src/Home.js
+++ app/src/Home.js
@@ -4,7 +4,13 @@ import AppNav from './AppNav.js';
 class Home extends Component {
     state = {  }
     render() { 
-        return ( <AppNav/> );
+        return ( <div>
+            <AppNav/> 
+            
+            <h2 style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
+                Welcome to a persolized movie tracking app!
+            </h2>
+        </div> );
     }
 }
  
--- app/src/Movies.js
+++ app/src/Movies.js
@@ -1,9 +1,59 @@
 import React, { Component } from 'react';
 import AppNav from './AppNav';
+import DatePicker from 'react-datepicker';
+import './App.css';
+import "react-datepicker/dist/react-datepicker.css";
+import {Link} from 'react-router-dom';
+import {
+    Form,
+    Input,
+    Button,
+    FormGroup,
+    Container,
+    Label
+  } from 'reactstrap';
+
 class Movies extends Component {
     state = {  }
     render() { 
-        return ( <AppNav/> );
+        const title= <h3>Add Movie</h3>
+        return ( 
+            <div>
+            <AppNav/> 
+            <Container>
+                {title}
+                <Form>
+                    <FormGroup>
+                        <Label for="title">Title</Label>
+                        <Input type="text" name="title" id="title" onChange={this.handleChange}/>
+                    </FormGroup>
+
+                    <FormGroup>
+                        <Label for="genre">Genre</Label>
+                        <Input type="text" name="genre" id="genre" onChange={this.handleChange}/>
+                    </FormGroup>
+
+                    <FormGroup>
+                        <Label for="movieDate">Movie Date</Label>
+                        <DatePicker selected={this.state.date} onChange={this.handleChange}/>
+                    </FormGroup>
+
+                    <FormGroup>
+                        <Label for="exampleText">Text Area</Label>
+                        <Input type="textarea" name="text" id="exampleText" />
+                    </FormGroup>
+
+                    <FormGroup>
+                        <Button color="primary" type="submit">Save</Button>
+                        <Button color="secondary" tag={Link} to="/genres">Cancel</Button>
+                    </FormGroup>
+
+
+
+                </Form>
+            </Container>
+        </div>
+        );
     }
 }
  
